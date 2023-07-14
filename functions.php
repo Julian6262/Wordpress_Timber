63ec24a8ceddfec51bdf43a717e83f847e49dee2
Julian6262 <d.baburin62@gmail.com>
2023-07-14 17:16:22 +0300

    wp

--- functions.php
+++ functions.php
@@ -3,6 +3,7 @@
 
 add_action( 'wp_enqueue_scripts', 'timber_style' );
 function timber_style() {
-	wp_enqueue_style( 'style-name', get_stylesheet_uri() );
+//     wp_enqueue_style( 'magnific', get_template_directory_uri() . '/assets/css/magnific-popup.css');
+	wp_enqueue_style( 'main-style', get_stylesheet_uri() );
 // 	wp_enqueue_script( 'script-name', get_template_directory_uri() . '/js/example.js', array(), '1.0.0', true );
 }
